095337ded3c59671132713a1a610c9cc51024431
salihtaniyici <salihtaniyici@users.noreply.github.com>
2023-03-26 19:54:12 +0300

    while-foreach

---
+++ while-foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace while_foreach;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("While");
+        Console.Write("Give me a number: ");
+        int limit = int.Parse(Console.ReadLine());
+        int sum = 0;
+        int number = 1;
+        while (number <= limit)
+        {
+            sum += number;
+            number++;
+        }
+        Console.WriteLine("Sum: " + sum);
+
+        Console.WriteLine("Foreach");
+        string[] nationality = {"Turkey","France","England"};
+        foreach (var nat in nationality)
+        {
+            Console.WriteLine(nat);
+        }
+    }
+}
