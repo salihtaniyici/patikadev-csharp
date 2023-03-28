57b5cd21a55f2b989bfbbda9d1d40bbb1a05c55a
salihtaniyici <salihtaniyici@users.noreply.github.com>
2023-03-28 14:58:36 +0300

    static-class

---
+++ static-class/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace static_class;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Player ID: {0}",Player.Id);
+
+        Player player01 = new Player("Salih","C#");
+        Console.WriteLine("Player ID: {0}",Player.Id);
+        Player player02 = new Player("X","C#");
+        Console.WriteLine("Player ID: {0}",Player.Id);
+        Player player03 = new Player("Y","C#");
+        Console.WriteLine("Player ID: {0}",Player.Id);
+
+        Console.WriteLine(Calculator.Sum(100,2));
+        Console.WriteLine(Calculator.Multiplication(5,9));
+    }
+}
+
+class Player
+{
+
+    static int id;
+
+    string name;
+    string team;
+
+    public static int Id { get => id;}
+
+    static Player()
+    {
+        id = 0;
+    }
+
+    public Player(string name,string team)
+    {
+        this.name = name;
+        this.team = team;
+        id++;
+    }
+}
+
+static class Calculator
+{
+
+    public static long Sum(int number01,int number02)
+    {
+        return number01 + number02;
+    }
+
+    public static long Multiplication(int number01,int number02)
+    {
+        return number01 * number02;
+    }
+
+
+}
