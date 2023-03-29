e1932382080c84f78e93a405791c1fa08490cfe6
salihtaniyici <salihtaniyici@users.noreply.github.com>
2023-03-29 12:23:26 +0300

    enums

---
+++ enums/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace enums;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Day.Saturday);
+        Console.WriteLine((int)Day.Thursday);
+
+        int input = 12;
+
+        if (input == (int)Player.Idle)
+        {
+            Console.WriteLine("Player IDLE");
+        }
+
+        if (input == (int)Player.Walk)
+        {
+            Console.WriteLine("Player WALK");
+        }
+
+        if (input == (int)Player.Jump)
+        {
+            Console.WriteLine("Player JUMP");
+        }
+
+        if (input == (int)Player.Attack)
+        {
+            Console.WriteLine("Player ATTACK");
+        }
+    }
+}
+
+enum Day
+{
+    Monday,
+    Tuesday,
+    Wednesday = 7,
+    Thursday,
+    Friday = 23,
+    Saturday,
+    Sunday
+
+}
+
+enum Player
+{
+    Idle = 2,
+    Walk = 6,
+    Jump = 9,
+    Attack = 12
+
+}
